2a9ae7c59cf055bb3861bed1a3e61c1be8c1dde3
Ahmad zaky <53550696+saveitlol@users.noreply.github.com>
2023-11-13 21:52:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
 <?php include("db.php"); 
-$product_id = '';
-$product_name= '';
-$stock= '';
-$price= '';
-  if  (isset($_GET['id'])) {
-  $id = $_GET['id'];
-  $query = "SELECT * FROM tb_product WHERE id=$id";
-  $result = mysqli_query($conn, $query);
-  if (mysqli_num_rows($result) == 1) {
-    $row = mysqli_fetch_array($result);
-    $product_id = $row['product_id'];
-    $product_name = $row['product_name'];
-    $stock = $row['stock'];
-    $price = $row['price'];
-
-  }
-}
-
-if (isset($_POST['update'])) {
-  $id = $_GET['id'];
-  $product_id = $_POST['product_id'];
-  $product_name = $_POST['product_name'];
-  $stock = $_POST['stock'];
-  $price = $_POST['price'];
-  $query = "UPDATE tb_product set product_id = '$product_id', product_name = '$product_name', stock = '$stock', price = '$price' WHERE id=$id";
-  mysqli_query($conn, $query);
-  $_SESSION['message'] = 'Task Updated Successfully';
-  $_SESSION['message_type'] = 'warning';
-  header('Location: index.php');
-}
 ?>
 
 <?php include('includes/header.php'); ?>
@@ -111,7 +81,7 @@ if (isset($_POST['update'])) {
               <a href="" data-bs-toggle="modal"data-bs-target="#modal<?php echo $row['id']?>"class="btn btn-secondary">
                 <i class="fa fa-solid fa-marker"></i>
               </a>
-              <a href="update.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+              <a href="update-old.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
                 <i class="fa fa-solid fa-marker"></i>
               </a>
               <!-- onclick="return confirm('Are you sure?')" -->
@@ -146,10 +116,8 @@ if (isset($_POST['update'])) {
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                           </button>
                       </div>
-                      <!-- di dalam modal-body terdapat 4 form input yang berisi data.
-                      data-data tersebut ditampilkan sama seperti menampilkan data pada tabel. -->
                       <div class="modal-body">
-                          <form method="POST" >
+                          <form action="<?php echo $_GET['id']; ?>" method="POST" >
                               <div class="form-group">
                                   <label for="exampleFormControlInput1">Nama Barang</label>
                                   <input type="text" class="form-control" value="<?php echo $row['product_id']; ?>">
@@ -170,10 +138,11 @@ if (isset($_POST['update'])) {
                                   <label for="exampleFormControlInput1">Harga Barang</label>
                                   <input type="text" class="form-control" value="<?php echo $row['price']; ?>">
                               </div>
+                              <input type="submit" name="update" class="btn btn-success btn-block" value="Save">
                           </form>
                       </div>
                       <div class="modal-footer">
-                          <button type="button" class="btn btn-primary" name="update">Save changes</button>
+                          <button type="submit" class="btn btn-primary" name="update" value="Save">Save changes</button>
                       </div>
                   </div>
               </div>
